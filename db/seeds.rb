72d9c4ee4abaf8e997329f2856dd9747a356c8e1
Eric Tran <eric.tran119@gmail.com>
2021-07-09 14:12:39 -0400

    fixing seeding issues

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,5 +6,14 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
+User.destroy_all
+User.reset_pk_sequence
+Photo.destroy_all
+Photo.reset_pk_sequence
 
-guestUser = User.create(email: 'guestUser@creatr.com', password: 'password', first_name: 'Guest', last_name: 'User', age: '25')
+
+guestUser = User.create!(email: 'guestUser@creatr.com', password: 'password', first_name: 'Guest', last_name: 'User', age: '25')
+photo = Photo.create!(author_id: 1).photo.attach(
+  io: File.open("app/assets/images/splash_background7.jpg"), filename: "splash_background7.jpg"
+  )
+

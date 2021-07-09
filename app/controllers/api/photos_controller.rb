9580783f35a4d20d214604bdca54fab5f38f263a
Eric Tran <eric.tran119@gmail.com>
2021-07-09 13:40:42 -0400

    added filing to photo mvp

--- app/controllers/api/photos_controller.rb
+++ app/controllers/api/photos_controller.rb
@@ -1,8 +1,14 @@
 class Api::PhotosController < ApplicationController
 
+  def index 
+    @photos = Photo.all
+    # render :index
+    # dont need render bc sending json request from frontend  and response will begin jbuilder
+  end
+
   def show
     @photo = Photo.find(params[:id])
-    render :show
+    # render :show
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   namespace :api, defaults: {format: :json} do
     resource :user, only: [:create, :show]
     resource :session, only: [:create, :destroy, :show]
-    resources :photos, only: [:show]
+    resources :photos, only: [:show, :index]
   end
   # resources :photos, only: :show
 

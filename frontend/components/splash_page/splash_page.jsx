0617e910c10e2c9a5296e452a3b417a213f2d34b
Eric Tran <eric.tran119@gmail.com>
2021-07-12 15:22:24 -0400

    added animation css

--- frontend/components/splash_page/splash_page.jsx
+++ frontend/components/splash_page/splash_page.jsx
@@ -4,9 +4,7 @@ import { Link } from 'react-router-dom';
 
 const SplashMessage = () => (
   <div className="splashMsg">
-    <div className="splashMsg1">
-    <h1>Find your inspiration.</h1>
-    </div>
+    <h1 className="splashMsg1">Find your inspiration.</h1>
     <div className="splashMsg2">
     <h2>Join the Creatr community, home to tens of billions of photos and unlimited creators! </h2>
     </div>

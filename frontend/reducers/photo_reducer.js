98925cd6d7a394160069bc86da74836d2e9a741a
Eric Tran <eric.tran119@gmail.com>
2021-07-14 18:44:25 -0400

    revised photo reducers and added comment reducers

--- frontend/reducers/photo_reducer.js
+++ frontend/reducers/photo_reducer.js
@@ -1,15 +1,18 @@
-import { RECEIVE_ALL_PHOTOS, RECEIVE_PHOTO} from "../actions/photo_actions";
+import { RECEIVE_ALL_PHOTOS, RECEIVE_PHOTO, REMOVE_PHOTO} from "../actions/photo_actions";
 
 //reducer is the filter
 const photoReducer = (oldState = {}, action) => {
   Object.freeze(oldState)
-  let nextState = Object.assign({}, oldState)
+  let nextState = Object.assign({}, oldState);
 
-  switch (action.type) { // action = line 5-8 in photo_actions
+  switch (action.type) { // action = RECEIVE_ALL_PHOTOS in photo_actions
     case RECEIVE_ALL_PHOTOS:
       return action.photos // returns line 7 in photo_actions
     case RECEIVE_PHOTO:
       nextState[action.photo.id] = action.photo
+      return nextState;
+    case REMOVE_PHOTO:
+      delete nextState[action.photoId]
       return nextState;
     default:
       return oldState;

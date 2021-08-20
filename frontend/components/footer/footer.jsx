a9e846a44d059719730a7de27586a0cc7cd5da32
Eric Tran <eric.tran119@gmail.com>
2021-08-20 18:24:54 -0400

    footer icons

--- frontend/components/footer/footer.jsx
+++ frontend/components/footer/footer.jsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import { FaLinkedin } from 'react-icons/fa';
+import { FaLinkedin, FaAngellist} from 'react-icons/fa';
 
 
 const Footer = () => (
   <footer className="bottomfooter">
     <h1>LinkedIn</h1>
     <h3> <FaLinkedin /> </h3>
+    <h3> <FaAngellist /> </h3>
     <h2>GitHub</h2>
   </footer>
 )

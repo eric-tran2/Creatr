0407961796607a44c26b3598514b176ba688ccf7
Eric Tran <eric.tran119@gmail.com>
2021-08-20 18:24:24 -0400

    added footer css

--- frontend/components/footer/footer.jsx
+++ frontend/components/footer/footer.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { FaLinkedin } from 'react-icons/fa';
 
 
 const Footer = () => (
   <footer className="bottomfooter">
     <h1>LinkedIn</h1>
+    <h3> <FaLinkedin /> </h3>
     <h2>GitHub</h2>
   </footer>
 )

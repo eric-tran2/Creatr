c6a48f8cb4cc94dc61c2e5dd86d54f0a282b3e16
Eric Tran <eric.tran119@gmail.com>
2021-08-20 18:25:47 -0400

    added footer for github

--- frontend/components/footer/footer.jsx
+++ frontend/components/footer/footer.jsx
@@ -1,14 +1,13 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import { FaLinkedin, FaAngellist} from 'react-icons/fa';
+import { FaLinkedin, FaAngellist, FaGithub} from 'react-icons/fa';
 
 
 const Footer = () => (
   <footer className="bottomfooter">
-    <h1>LinkedIn</h1>
     <h3> <FaLinkedin /> </h3>
     <h3> <FaAngellist /> </h3>
-    <h2>GitHub</h2>
+    <h3> <FaGithub /> </h3>
   </footer>
 )
 

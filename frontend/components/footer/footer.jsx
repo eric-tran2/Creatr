fe0a4fd59f170f854d0313ccb804383e5cf7d864
Eric Tran <eric.tran119@gmail.com>
2021-08-20 20:05:31 -0400

    added links to footer

--- frontend/components/footer/footer.jsx
+++ frontend/components/footer/footer.jsx
@@ -5,9 +5,9 @@ import { FaLinkedin, FaAngellist, FaGithub} from 'react-icons/fa';
 
 const Footer = () => (
   <footer className="bottomfooter">
-    <h3> <FaLinkedin /> </h3>
-    <h3> <FaAngellist /> </h3>
-    <h3> <FaGithub /> </h3>
+    <a href="https://www.linkedin.com/in/eric-tran-6aab70130/"><FaLinkedin /></a>
+    <a href="https://angel.co/u/eric-tran-28"><FaAngellist/></a>
+    <a href="https://github.com/eric-tran2/Creatr"><FaGithub/></a>
   </footer>
 )
 

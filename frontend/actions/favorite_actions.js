3168283a1c21906e156c65c8a157b62796d6e235
Eric Tran <eric.tran119@gmail.com>
2021-08-31 16:53:04 -0400

    favorites util

--- frontend/actions/favorite_actions.js
+++ frontend/actions/favorite_actions.js
@@ -1,4 +1,5 @@
 import * as FavoriteUtil from '../util/favorite_util'
 
 export const RECEIVE_ALL_FAVORITES = 'RECEIVE_ALL_FAVORITES';
-export const RECEIVE_ALL_FAVORITES = 'RECEIVE_ALL_FAVORITES';
+export const RECEIVE_FAVORITE = 'RECEIVE_FAVORITE';
+export const REMOVE_FAVORITES = 'REMOVE_FAVORITE';

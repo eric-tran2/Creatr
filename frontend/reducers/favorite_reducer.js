d54464e85f7c900606f3ddda3e898a0e59a02dc4
Eric Tran <eric.tran119@gmail.com>
2021-09-01 17:35:29 -0400

    on favorite reducers

--- frontend/reducers/favorite_reducer.js
+++ frontend/reducers/favorite_reducer.js
@@ -1,1 +1,7 @@
 import { RECEIVE_ALL_FAVORITES, RECEIVE_FAVORITE, REMOVE_FAVORITE } from "../actions/favorite_actions";
+
+
+const favoriteReducer = (oldState = {}, action) => {
+  Object.freeze(oldState)
+  let nextState = Object.assign({}, oldState);
+}

2963656db885780a0198e1bfc4d7ecc8a2f209d2
Eric Tran <eric.tran119@gmail.com>
2021-09-10 20:03:42 -0400

    added favorites reducer to entities

--- frontend/reducers/entities_reducer.js
+++ frontend/reducers/entities_reducer.js
@@ -3,9 +3,11 @@ import { combineReducers } from 'redux';
 import users from './users_reducer';
 import photos from './photo_reducer';
 import comments from './comment_reducer'
+import favorites from './favorite_reducer'
 
 export default combineReducers({
   users,
   photos,
   comments,
+  favorites,
 });

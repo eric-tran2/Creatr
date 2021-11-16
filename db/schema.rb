d87016ef07619de26b9cbffcea581e44a7e3baba
Eric Tran <eric.tran119@gmail.com>
2021-11-16 00:42:21 -0500

    tags table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_08_18_094254) do
+ActiveRecord::Schema.define(version: 2021_11_16_050747) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -63,6 +63,14 @@ ActiveRecord::Schema.define(version: 2021_08_18_094254) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["author_id"], name: "index_photos_on_author_id"
+  end
+
+  create_table "tags", force: :cascade do |t|
+    t.integer "photo_id", null: false
+    t.string "name", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["photo_id"], name: "index_tags_on_photo_id"
   end
 
   create_table "users", force: :cascade do |t|

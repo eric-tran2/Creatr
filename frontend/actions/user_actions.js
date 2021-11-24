ec2108a3f376bf99987ff5759244d9f01b1553b5
Eric Tran <eric.tran119@gmail.com>
2021-11-23 23:49:21 -0500

    front end for users

--- frontend/actions/user_actions.js
+++ frontend/actions/user_actions.js
@@ -1,13 +1,24 @@
 import * as UserUtil from '../util/user_util'
 
+export const RECEIVE_ALL_USERS = 'RECEIVER_ALL_USERS'
 export const RECEIVE_USER = 'RECEIVE_USER'
+
+
+export const receiveAllUsers = users => ({
+  type: RECEIVE_ALL_USERS,
+  users
+})
 
 export const receiveUser = user => ({
   type: RECEIVE_USER,
   user
 })
 
+export const requestUsers = () => dispatch => {
+  return UserUtil.fetchUsers().then(users => dispatch(receiveAllUsers(users)))
+}
+
 export const requestUser = userId => dispatch => {
-  return UserUtil.requestUser(userId).then(user => dispatch(receiveUser(user)))
+  return UserUtil.fetchUser(userId).then(user => dispatch(receiveUser(user)))
 }
 
